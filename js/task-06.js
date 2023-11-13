d961031d5420835401cbce9e35e55cdcf2cee824
urraccon <alexandru_marian1998@yahoo.com>
2023-11-13 14:35:39 +0200

    i changed the validation condition to task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,6 @@
 const inputField = document.getElementById("validation-input");
 inputField.addEventListener("blur", (text) => {
-  if (inputField.value.length < 6) {
+  if (inputField.value.length !== 6) {
     inputField.classList.add("invalid");
     inputField.classList.remove("valid");
   } else {
