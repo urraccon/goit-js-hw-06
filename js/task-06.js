8f18003b9b48e2d87f83f20c1b89c98bedbc7f2d
urraccon <133591634+urraccon@users.noreply.github.com>
2023-11-14 06:02:23 +0200

    added the number attribute to dataset.length

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,8 @@
 const inputField = document.getElementById("validation-input");
 inputField.addEventListener("blur", (text) => {
-  if (inputField.value.length != inputField.dataset.length) {
-    // debugger;
+  debugger;
+  if (inputField.value.length !== Number(inputField.dataset.length)) {
+    debugger;
     inputField.classList.add("invalid");
     inputField.classList.remove("valid");
   } else {
